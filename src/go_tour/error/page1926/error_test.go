a28581baef51f6578439492d484257b5d2b8a03d
shishuwu <shuwu.shi@gmail.com>
2019-11-25 21:34:09 +0800

    error

---
+++ src/go_tour/error/page1926/error_test.go
@@ -0,0 +1,27 @@
+package errortest
+
+import (
+	"fmt"
+	"time"
+	"testing"
+)
+
+type MyError struct {
+	When time.Time
+	What string
+}
+
+func (e *MyError) Error() string {
+	return fmt.Sprintf("at %v, %s", e.When, e.What)
+}
+
+func run() error {
+	return &MyError{time.Now(), "it didn't work"}
+}
+
+
+func Test(t *testing.T) {
+	if err := run(); err != nil {
+		fmt.Println(err)
+	}
+}
