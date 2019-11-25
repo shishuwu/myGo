1332998f461312a523f1a83502e4ff9fdab14b9a
shishuwu <shuwu.shi@gmail.com>
2019-11-25 15:15:16 +0800

    go interface

---
+++ src/go_tour/interface/page0926/interfaces_test.go
@@ -0,0 +1,53 @@
+package interfacetest
+
+import (
+	"fmt"
+	"math"
+	"testing"
+)
+
+type MyFloat float64
+
+func (f MyFloat) Abs() float64 {
+	if f < 0 {
+		return float64(-f)
+	}
+
+	return float64(f)
+}
+
+// ------------------------------
+
+type Vertex struct {
+	X,Y float64
+}
+
+func (v *Vertex) Abs() float64 {
+	return math.Sqrt( v.X * v.X + v.Y *v.Y)
+}
+
+// ------------------------------
+
+type Abser interface {
+	Abs() float64
+}
+
+
+func Test(t *testing.T) {
+	var a Abser
+	f := MyFloat(-math.Sqrt2)
+	v := Vertex{3,4}
+
+	a = f
+	a = &v
+	// 下面一行，v 是一个 Vertex（而不是 *Vertex）
+	// 所以没有实现 Abser。
+	//a = v
+
+
+
+	
+	//fmt.Println(v.Abs())
+	fmt.Println(a.Abs())
+
+}
